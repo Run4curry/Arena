933f1fba1ca59a1c927279ec404f4693e0ed070d
Justin Huynh <juh079@ucsd.edu>
2017-01-26 16:19:21 -0800

    added comments

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*
- * Module dependencies 
+ * Module dependencies
  */
-var express = require('express'); 
+var express = require('express');
 var PORT = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
 var app = module.exports = express();
@@ -14,10 +14,15 @@ app.use(express.static(__dirname + '/public'));
 
 //render the index page, single page application with partials that
 //we will inject into the index page whenever the view needs to change
-//using Angular Routing to do this on the client side 
+//using Angular Routing to do this on the client side
+//index.index refers to the index variable above './routes/index', the
+//exports.index function inside that index.js file
 app.get('/', index.index);
 
 //video API
+//This app.use() express call handles all types of requests to /video route.
+//It handles the route by redirecting the user to the ./routes/videoupload.js
+//which then handles the different types of calls.
 app.use('/video', video);
 
 app.listen(PORT, function(){
--- routes/videoupload.js
+++ routes/videoupload.js
@@ -1,6 +1,6 @@
-//this file is responsible for all uploading of videos to AWS S3 
-//and retrieval of video URLs from AWS DynamoDB so we can display 
-//them to the client 
+//this file is responsible for all uploading of videos to AWS S3
+//and retrieval of video URLs from AWS DynamoDB so we can display
+//them to the client
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
@@ -8,15 +8,15 @@ var router = express.Router();
 var AWS = require('aws-sdk');
 
 
-//credentials for using AWS services 
+//credentials for using AWS services
 //NOTE TODO, ONLY PUSH TO GITHUB AND HOSTING SERVICE
-//ONCE THIS HIDDEN 
+//ONCE THIS HIDDEN
 var params = {
-    accessKeyId : '',
-    secretAccessKey : ''
+    accessKeyId : 'AKIAJ75XUCHM6C3HWFWQ',
+    secretAccessKey : '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'
 };
 
-var s3 = new AWS.S3(params);//create instance of s3 that will allow us to write the file to our cloud storage 
+var s3 = new AWS.S3(params);//create instance of s3 that will allow us to write the file to our cloud storage
 
 router.post('/', function(req,res){
     var uploadParams = {
@@ -25,14 +25,14 @@ router.post('/', function(req,res){
         Body: ''
     }
     console.log("swag");
-    var filestream = fs.createReadStream('./routes/harambe-22.jpg');//construct filestream to read in the file 
-    //error handling for filestream 
+    var filestream = fs.createReadStream('./routes/harambe-22.jpg');//construct filestream to read in the file
+    //error handling for filestream
     filestream.on('error', function(err){
         console.log("File error! FUck you!", err);
     });
     uploadParams.Body = filestream;
     uploadParams.Key = path.basename('./routes/harambe-22.jpg');//this file is being called from the directory above
-    //so we must put ourselves in the directory above 
+    //so we must put ourselves in the directory above
 
     s3.upload(uploadParams , function(err, data){
         if(err) {
@@ -43,7 +43,7 @@ router.post('/', function(req,res){
         }
     });
 
-    
+
 });
 
 
