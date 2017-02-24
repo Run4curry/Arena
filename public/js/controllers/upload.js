fd2e19f6b595f5bfb3eec47f148f78427e96ec0f
Shiva Kannan <s1kannan@ucsd.edu>
2017-02-23 19:00:12 -0800

    video/image uploading works kind of

--- public/js/controllers/upload.js
+++ public/js/controllers/upload.js
@@ -1,19 +1,36 @@
 //TODO this function sends the post request to where? What is $http referring
-//to? How will videoupload.js get a hold of this file? 
+//to? How will videoupload.js get a hold of this file?
+ 
 function uploadController($scope , $http){
     $scope.file;
 
     $scope.uploadFile = function(){
         console.log("I am in here!");
-        console.log($scope.file);
-        $http({
-            method: 'POST',
-            url: '/video' ,
-            data : {
-                file : $scope.file
-            }
-        }).success(function(data){
-            console.log(data);
+        console.log($scope.file.name);
+        var file = $scope.file;
+        var fd = new FormData();
+        fd.append('file',file);
+        //make a post request to the videoupload API function
+        /*var request = {
+            method: "POST",
+            url : '/video',
+            data: {
+                filename : $scope.file.name ,
+                dummyVar : 'a'
+            }*/
+            //headers : {'Content-Type' : undefined},
+            //transformRequest : angular.identity
+        //}; //variable to create the POST request to API
+
+        //make the request
+        //TODO experiment around a little more to check if  
+        //this can be rewritten a bit more 
+        $http.post('/video' , fd, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type' : undefined}
+        })
+        .success(function(data){
+                console.log(data);
         });
     }
 }
--- routes/videoupload.js
+++ routes/videoupload.js
@@ -1,38 +1,56 @@
-//this file is responsible for all uploading of videos to AWS S3
-//and retrieval of video URLs from AWS DynamoDB so we can display
-//them to the client
+//this file is responsible for all uploading of videos to AWS S3 
+//and retrieval of video URLs from AWS DynamoDB so we can display 
+//them to the client 
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
 var router = express.Router();
 var AWS = require('aws-sdk');
+var multer = require('multer');
+var upload = multer({dest: 'public/uploads/'});
 
 
-//credentials for using AWS services
+
+//use the multipartymiddleware middleware 
+
+//credentials for using AWS services 
 //NOTE TODO, ONLY PUSH TO GITHUB AND HOSTING SERVICE
 //ONCE THIS HIDDEN
+//set environment variables on Heroku: S3_URL DYNAMO_URL  
 var params = {
-    accessKeyId : '',
-    secretAccessKey : ''
+    
 };
 
-var s3 = new AWS.S3(params);//create instance of s3 that will allow us to write the file to our cloud storage
+var s3 = new AWS.S3(params);//create instance of s3 that will allow us to write the file to our cloud storage 
 
-router.post('/', function(req,res){
+router.post('/', upload.single('file') , function(req,res){
     var uploadParams = {
         Bucket : 'sdfdf',
         Key : '',
         Body: ''
-    }
+    };
+    //debugging statements 
+    console.log(req.body);
+    console.log(req.file);
+    console.log(req.file.path);
+
     console.log("swag");
-    var filestream = fs.createReadStream('./routes/harambe-22.jpg');//construct filestream to read in the file
-    //error handling for filestream
+    //the file that the user uploaded on the client side and that the server will upload 
+    //to S3 Bucket 
+    var filestream2 = fs.createReadStream(req.file.path);
+    var filestream = fs.createReadStream('./routes/harambe-22.jpg');//construct filestream to read in the file 
+    //error handling for filestream 
     filestream.on('error', function(err){
         console.log("File error! FUck you!", err);
     });
-    uploadParams.Body = filestream;
-    uploadParams.Key = path.basename('./routes/harambe-22.jpg');//this file is being called from the directory above
-    //so we must put ourselves in the directory above
+    //error handling for filestream 2
+    filestream2.on('error', function(err){
+        console.log("File error in filestream2!!");
+    });
+
+    uploadParams.Body = filestream2;
+    uploadParams.Key = path.basename(req.file.path);//this file is being called from the directory above
+    //so we must put ourselves in the directory above 
 
     s3.upload(uploadParams , function(err, data){
         if(err) {
@@ -40,10 +58,21 @@ router.post('/', function(req,res){
         }
         if(data){
             console.log("No Fuck You still!" , data.Location);
+            var response = {
+                file : req.file ,
+                location : data.Location
+            };
+            fs.unlinkSync(req.file.path);
+            /*TODO We want to store the data.Location variable in the DynamoDB database 
+            or SQL database, whatever we decide on implementing here regardless the 
+            method should be straightforward 
+            
+             */
+            res.json(response);
         }
     });
 
-
+    
 });
 
 
